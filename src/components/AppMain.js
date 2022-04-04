68d74c4f06c3eb940e6144059a5eea527ef4ff31
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-04 11:57:39 +0200

    extracting App layout features into components

---
+++ src/components/AppMain.js
@@ -0,0 +1,15 @@
+const AppMain = () => {
+  return (
+    <main>
+      <h2>Welcome to IronTravel!</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quaerat
+        eaque vitae aut adipisci expedita, est accusantium cum praesentium.
+        Nihil minus quidem corporis aspernatur iusto vitae non minima provident
+        veritatis.
+      </p>
+    </main>
+  )
+}
+
+export default AppMain
