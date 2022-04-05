fd52e9158747fbca95f3839eba0a82d3521e302f
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-05 16:46:53 +0200

    checkboxes 'buttons' exist but are inert

--- src/components/countries/CountryList.js
+++ src/components/countries/CountryList.js
@@ -10,6 +10,23 @@ const CountryList = () => {
   return (
     <section className='CountryListSection'>
       <h2>All the Countries</h2>
+      <form>
+        <p>Filters:</p>
+        <div className='filter-group'>
+          <input type='checkbox' name='europe' id='europe' />
+          <label htmlFor='europe'>Europe</label>
+          <input type='checkbox' name='north-america' id='north-america' />
+          <label htmlFor='north-america'>North America</label>
+          <input type='checkbox' name='south-america' id='south-america' />
+          <label htmlFor='south-america'>South America</label>
+          <input type='checkbox' name='africa' id='africa' />
+          <label htmlFor='africa'>Africa</label>
+          <input type='checkbox' name='oceania' id='oceania' />
+          <label htmlFor='oceania'>Oceania</label>
+          <input type='checkbox' name='antarctica' id='antarctica' />
+          <label htmlFor='antarctica'>Antarctica</label>
+        </div>
+      </form>
       <div className='CountryList'>{countryInfoCards}</div>
     </section>
   )
