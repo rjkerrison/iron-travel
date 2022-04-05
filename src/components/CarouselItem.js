a6c494bfa4079e0df855a1602a7eda8130f8f3de
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-05 10:13:51 +0200

    basic state

--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -1,8 +1,19 @@
 import './CarouselItem.css'
 
+import { useState } from 'react'
+
 const CarouselItem = ({ heading, description, backgroundImageUrl }) => {
+  // we use useState inside a component
+  // to create stateful data which we can change
+  // with the setter, and so React will rerender this component
+  const [data, setData] = useState('is not favourited :(')
+
   const setFavouriteCity = () => {
-    alert(`You favourited ${heading}!`)
+    if (data === 'is favourited!!!') {
+      setData('YOU ALREADY CLICKED')
+    } else {
+      setData('is favourited!!!')
+    }
   }
 
   return (
@@ -10,7 +21,9 @@ const CarouselItem = ({ heading, description, backgroundImageUrl }) => {
       className='CarouselItem'
       style={{ backgroundImage: `url(${backgroundImageUrl})` }}
     >
-      <h3>{heading}</h3>
+      <h3>
+        {heading} {data}
+      </h3>
       <p>{description}</p>
       <button className='favourite' onClick={setFavouriteCity}>
         ⭐️
