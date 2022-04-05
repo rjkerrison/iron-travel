ebf42dd9b9ddc881dc0d237a4213df3cea6d5b29
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-05 17:11:54 +0200

    filters work, using useEffect and managed forms

--- src/components/countries/CountryList.js
+++ src/components/countries/CountryList.js
@@ -1,11 +1,49 @@
+import { useEffect, useState } from 'react'
 import countries from '../../assets/countries.json'
 import CountryInfoCard from './CountryInfoCard'
 import './CountryList.css'
 
 const CountryList = () => {
-  const countryInfoCards = countries.map((country) => {
+  const [selectedCountries, setSelectedCountries] = useState(countries)
+  const [formData, setFormData] = useState({
+    europe: false,
+    southAmerica: false,
+    northAmerica: false,
+    africa: false,
+    antarctica: true,
+    oceania: false,
+  })
+
+  const countryInfoCards = selectedCountries.map((country) => {
     return <CountryInfoCard {...country} />
   })
+
+  const updateFilters = () => {
+    const selectedContinents = Object.entries(formData)
+      .filter(([k, v]) => v)
+      .map(([k, v]) => k.toLowerCase())
+
+    const newCountries = countries.filter((c) =>
+      selectedContinents.includes(
+        c.continents[0].replace(/\s/g, '').toLowerCase()
+      )
+    )
+
+    setSelectedCountries(newCountries)
+  }
+
+  useEffect(updateFilters, [formData])
+
+  const handleCheck = (e) => {
+    const name = e.target.name
+
+    const nfd = {
+      ...formData,
+      [name]: !formData[name],
+    }
+    console.log(nfd)
+    setFormData(nfd)
+  }
 
   return (
     <section className='CountryListSection'>
@@ -13,17 +51,53 @@ const CountryList = () => {
       <form>
         <p>Filters:</p>
         <div className='filter-group'>
-          <input type='checkbox' name='europe' id='europe' />
+          <input
+            type='checkbox'
+            name='europe'
+            id='europe'
+            checked={formData.europe}
+            onClick={handleCheck}
+          />
           <label htmlFor='europe'>Europe</label>
-          <input type='checkbox' name='north-america' id='north-america' />
+          <input
+            type='checkbox'
+            name='northAmerica'
+            id='north-america'
+            checked={formData.northAmerica}
+            onClick={handleCheck}
+          />
           <label htmlFor='north-america'>North America</label>
-          <input type='checkbox' name='south-america' id='south-america' />
+          <input
+            type='checkbox'
+            name='southAmerica'
+            id='south-america'
+            checked={formData.southAmerica}
+            onClick={handleCheck}
+          />
           <label htmlFor='south-america'>South America</label>
-          <input type='checkbox' name='africa' id='africa' />
+          <input
+            type='checkbox'
+            name='africa'
+            id='africa'
+            checked={formData.africa}
+            onClick={handleCheck}
+          />
           <label htmlFor='africa'>Africa</label>
-          <input type='checkbox' name='oceania' id='oceania' />
+          <input
+            type='checkbox'
+            name='oceania'
+            id='oceania'
+            checked={formData.oceania}
+            onClick={handleCheck}
+          />
           <label htmlFor='oceania'>Oceania</label>
-          <input type='checkbox' name='antarctica' id='antarctica' />
+          <input
+            type='checkbox'
+            name='antarctica'
+            id='antarctica'
+            checked={formData.antarctica}
+            onClick={handleCheck}
+          />
           <label htmlFor='antarctica'>Antarctica</label>
         </div>
       </form>
