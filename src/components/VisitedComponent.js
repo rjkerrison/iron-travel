dd93073a5df251fa63163160f2b3d05fd5735503
Noa <noa.benaziz@gmail.com>
2022-04-08 13:02:41 +0200

    start of a frontend

---
+++ src/components/VisitedComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+const VisitedComponent = () => {
+
+    
+    
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default VisitedComponent
